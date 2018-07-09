cca84cfaa2314c2f5f12c29e78d3c84c31ff58a0
Rex.Lei <1203208955@qq.com>
2018-07-09 20:45:19 +0800

    bugfix

--- kangaroo-web-admin/src/main/java/io/github/pactstart/admin/system/form/RoleUserChangeForm.java
+++ kangaroo-web-admin/src/main/java/io/github/pactstart/admin/system/form/RoleUserChangeForm.java
@@ -1,8 +1,13 @@
 package io.github.pactstart.admin.system.form;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.validation.constraints.NotNull;
 import java.util.List;
 
+@Getter
+@Setter
 public class RoleUserChangeForm {
 
     @NotNull(message = "roleId不能为空")
