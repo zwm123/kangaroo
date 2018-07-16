cd50aa82e8f8434f2371b389d08508f32e611eda
Rex.Lei <1203208955@qq.com>
2018-07-16 18:43:15 +0800

    优化

--- pay-spring-boot-starter/src/main/java/io/github/pactrex/pay/wxpay/enums/WxPayTradeTypeEnum.java
+++ pay-spring-boot-starter/src/main/java/io/github/pactrex/pay/wxpay/enums/WxPayTradeTypeEnum.java
@@ -20,5 +20,22 @@ public enum WxPayTradeTypeEnum {
     /**
      * 刷卡支付，刷卡支付有单独的支付接口，不调用统一下单接口
      */
-    MICROPAY
+    MICROPAY;
+
+    /**
+     * 默认valueOf找不到匹配项会抛出异常，这里返回空
+     *
+     * @param tradeType
+     * @return
+     */
+    public static WxPayTradeTypeEnum valueOfName(String tradeType) {
+        WxPayTradeTypeEnum result = null;
+        for (WxPayTradeTypeEnum item : values()) {
+            if (item.equals(tradeType)) {
+                result = item;
+                break;
+            }
+        }
+        return result;
+    }
 }
