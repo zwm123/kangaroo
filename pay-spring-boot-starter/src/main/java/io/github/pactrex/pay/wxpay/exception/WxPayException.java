628f8cfc8aed9714687cec786cc3d91876de6bc3
Rex.Lei <1203208955@qq.com>
2018-07-17 22:54:26 +0800

    rename package

--- pay-spring-boot-starter/src/main/java/io/github/pactrex/pay/wxpay/exception/WxPayException.java
+++
@@ -1,8 +0,0 @@
-package io.github.pactrex.pay.wxpay.exception;
-
-public class WxPayException extends RuntimeException {
-
-    public WxPayException(String message) {
-        super(message);
-    }
-}
---
+++ pay-spring-boot-starter/src/main/java/io/github/pactstart/pay/alipay/autoconfigure/AliPayAutoConfiguration.java
@@ -0,0 +1,21 @@
+package io.github.pactstart.pay.alipay.autoconfigure;
+
+import io.github.pactstart.pay.alipay.AliPayService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(AliPayConfig.class)
+public class AliPayAutoConfiguration {
+
+    @Autowired
+    private AliPayConfig aliPayConfig;
+
+    @Bean
+    public AliPayService aliPayService() {
+        return new AliPayService(aliPayConfig);
+    }
+
+}
