07428b01bfce90c860589142dd891dd752425a1c
Rex.Lei <1203208955@qq.com>
2018-08-02 13:32:33 +0800

    支持classpath配置路径

--- aliyun-sts-spring-boot-starter/src/main/java/io/github/pactstart/sts/autoconfigure/AliyunSTSAutoConfiguration.java
+++ aliyun-sts-spring-boot-starter/src/main/java/io/github/pactstart/sts/autoconfigure/AliyunSTSAutoConfiguration.java
@@ -1,5 +1,7 @@
 package io.github.pactstart.sts.autoconfigure;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,8 +12,10 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
@@ -25,11 +29,43 @@ public class AliyunSTSAutoConfiguration implements InitializingBean, Application
     @Autowired
     private ApplicationContext applicationContext;
 
+    private static Logger logger = LoggerFactory.getLogger(AliyunSTSAutoConfiguration.class);
+
     @Override
     public void afterPropertiesSet() throws Exception {
         Map<String, ProductConfig> productConfigMap = new HashMap<>();
         productConfigMap.put("oss", stsConfig.getOss());
         registerSTSClientBeanDefinition(productConfigMap);
+    }
+
+    public static String readFileFromClasspath(String url) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        BufferedReader bufferedReader = null;
+
+        try {
+            ClassPathResource classPathResource = new ClassPathResource(url);
+            InputStreamReader inputStreamReader = new InputStreamReader(classPathResource.getInputStream());
+            bufferedReader = new BufferedReader(inputStreamReader);
+            String line;
+
+            String lineSeparator = System.getProperty("line.separator");
+            while ((line = bufferedReader.readLine()) != null) {
+                stringBuilder.append(line).append(lineSeparator);
+            }
+        } catch (Exception e) {
+            logger.debug(String.format("Failed to load file from url: %s: %s", url, e.getMessage()));
+            return null;
+        } finally {
+            if (bufferedReader != null)
+                try {
+                    bufferedReader.close();
+                } catch (IOException e) {
+                    logger.debug(String.format("Unable to close buffered reader.. %s", e.getMessage()));
+                }
+        }
+
+        return stringBuilder.toString();
     }
 
     private void registerSTSClientBeanDefinition(Map<String, ProductConfig> productConfigMap) throws Exception {
@@ -44,7 +80,14 @@ public class AliyunSTSAutoConfiguration implements InitializingBean, Application
 
                 ProductConfig productConfig = entry.getValue();
 
-                String policy = readToString(productConfig.getPolicyFile());
+                String policy = null;
+                if (productConfig.getPolicyFile().contains("classpath")) {
+                    policy = readFileFromClasspath(productConfig.getPolicyFile());
+                } else {
+                    policy = readToString(productConfig.getPolicyFile());
+                }
+                logger.info("policy file path:{}", productConfig.getPolicyFile());
+                logger.info("policy file content:{}", policy);
 
                 builder.getBeanDefinition().getPropertyValues().addPropertyValue("policy", policy);
                 builder.getBeanDefinition().getPropertyValues().addPropertyValue("productConfig", productConfig);
