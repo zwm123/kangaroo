f54540a376bea1ec467087a4fb9b3955a2c90bec
Rex.Lei <1203208955@qq.com>
2018-09-15 11:09:12 +0800

    update

--- kangaroo-simple-web-framework/src/main/java/io/github/pactstart/simple/web/framework/common/form/PageForm.java
+++ kangaroo-simple-web-framework/src/main/java/io/github/pactstart/simple/web/framework/common/form/PageForm.java
@@ -3,6 +3,7 @@ package io.github.pactstart.simple.web.framework.common.form;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 
 @Getter
@@ -13,6 +14,7 @@ public class PageForm {
     private int pageNum = 1;
 
     @Min(value = 1, message = "每页展示数量不合法")
+    @Max(value = 100, message = "每页最多展示100条")
     private int pageSize = 10;
 
 }
